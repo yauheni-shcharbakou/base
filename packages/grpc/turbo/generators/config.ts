508f40de7bd69b39a092c64fff347438375140f4
Evgenii Shcherbakov <iipekolict@gmail.com>
2026-02-04 23:39:14 +0400

    redeploy

--- packages/grpc/turbo/generators/config.ts
+++ packages/grpc/turbo/generators/config.ts
@@ -1,6 +1,6 @@
 import { PlopTypes } from '@turbo/gen';
 import { compileGenerator } from './compile';
 
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
+export default function generator(plop: PlopTypes.NodePlopAPI) {
   [compileGenerator].forEach((generator) => generator(plop));
 }
